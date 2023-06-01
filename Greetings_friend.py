8cb3753d21de92a95e56e74861e448811d90d8a2
Sentinel Magnus <ekko2363@gmail.com>
2023-06-02 01:21:27 +0500

    fix

--- Greetings_friend.py
+++ Greetings_friend.py
@@ -1,6 +1,7 @@
 def Greetings():
     print('----------PHONEBOOK----------')
     name = input('- Please enter your name:\n  ')
-    print(f'- Hello, {name}. How may I help you?')
+    secon_Name = input('- Please enter your second name:\n  ')
+    print(f'- Hello, {name} {secon_Name}. How may I help you?')
     print('------------------------------')
     
